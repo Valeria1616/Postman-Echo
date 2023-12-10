166a6160b6e9dce92cb9edc0e01d9cf3511bbc9f
Valeria V <volkovich-va@mail.ru>
2023-12-10 17:48:40 +0300

    Added build success

--- src/test/java/ru/netology/rest/CheckoutPostmanEchoTest.java
+++ src/test/java/ru/netology/rest/CheckoutPostmanEchoTest.java
@@ -20,7 +20,7 @@ public class CheckoutPostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("I Love JavaQ - Netology"))
+                .body("data", equalTo("I Love Java - Netology"))
         ;
     }
 }
