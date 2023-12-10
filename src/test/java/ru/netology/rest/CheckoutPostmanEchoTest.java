93675ae05fb59097ab6daa75dd38f0b3a4ebbdf7
Valeria V <volkovich-va@mail.ru>
2023-12-10 17:34:11 +0300

    Added test and gradle.yml

---
+++ src/test/java/ru/netology/rest/CheckoutPostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutPostmanEchoTest {
+    @Test
+    void shouldCheckoutPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("I Love Java - Netology (Нетология)") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("I Love Java - Netology (Нетология)"))
+        ;
+    }
+}
